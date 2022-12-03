1d1d5f0a17fb15877fd51fbf5e2a74a594015b40
RadhaDevarapalli <119493543+RadhaDevarapalli@users.noreply.github.com>
2022-12-03 13:09:06 +0530

    Add files via upload

---
+++ ForLoopDecrement.java
@@ -0,0 +1,14 @@
+package com.corejava;
+
+public class ForLoopDecrement {
+	public static void main(String[]args) {
+		for(int i=20;i>5;i--) {
+			System.out.println(i);
+		}
+		for(int i=50;i>0;i-=10) {
+			System.out.println(i);
+		}
+	
+	}
+
+}
